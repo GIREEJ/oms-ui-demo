ec5552d785cf17f7389955037795b427ab9c3476
giree <gireejsahu@gmail.com>
2026-01-01 09:43:20 +0530

    feat(ui): add base layout, routing, and page placeholders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Layout } from './shared/components/layout/layout';
+import { Home } from './features/dashboard/home/home';
+import { ProductsList } from './features/products/products-list/products-list';
+import { ProductsDetail } from './features/products/products-detail/products-detail';
+import { OrdersList } from './features/orders/orders-list/orders-list';
+import { OrdersDetails } from './features/orders/orders-details/orders-details';
+import { ReportsList } from './features/reports/reports-list/reports-list';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path:'',
+        component:Layout,
+        children:[
+            { path:'', component:Home },
+            { path:'products', component: ProductsList },
+            { path:'products/:id', component: ProductsDetail },
+            { path:'orders', component: OrdersList },
+            { path:'orders/:id', component: OrdersDetails },
+            { path:'reports', component: ReportsList }
+        ]
+    },
+    { path:'login', component:Login },
+    { path:'register', component:Register },
+    { path:'**', redirectTo:'' }
+];
---
+++ src/app/features/orders/orders-details/orders-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-details',
+  imports: [],
+  templateUrl: './orders-details.html',
+  styleUrl: './orders-details.scss',
+})
+export class OrdersDetails {
+
+}
